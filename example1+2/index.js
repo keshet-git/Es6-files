3c99c6fcad6913c64aa38c3719bd7c86e26a313a
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-16 12:51:00 +0300

    Add files via upload

---
+++ example1+2/index.js
@@ -0,0 +1,13 @@
+//let example1 =[1,2,3,4,5,6];
+//let example2 =[...example1];
+//example2.push(true);
+//console.log(example2);
+
+let example1 = {
+  firstName: 'Keshet'
+};
+let example2 = {
+  ...example1
+}
+
+console.log(example2)
