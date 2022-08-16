b13b5e2c114583fc311efba71eb2059ff4983947
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-16 12:53:21 +0300

    Add files via upload

---
+++ income/index.js
@@ -0,0 +1,16 @@
+/*
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for (const income of incomes) {
+  console.log(income);
+  total += income;
+}
+//console.log(total);
+*/
+
+let fullName = "Keshet Rachel Avrahami"
+
+for (const char of fullName) {
+  console.log(char);
+}
