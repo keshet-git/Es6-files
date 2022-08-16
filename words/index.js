9703939f6021877913a3c7d49c900f2d7bead1ce
keshet-git <83539396+keshet-git@users.noreply.github.com>
2022-08-16 12:56:14 +0300

    Add files via upload

---
+++ words/index.js
@@ -0,0 +1,12 @@
+let word1 = "Keshet";
+let word2 = "Avrahami";
+let num1 = 2;
+let num2 = 3;
+//const fullname = word1 + ' ' + word2;
+//const fullname = `${num1 + num2} ${word1} ${word2}`;
+//let example = 'Hello \n' + 'world';
+//let example = `${word1}
+//${word2}`;
+let example = `${word1} ${word2}`;
+console.log(example);
+//document.getElementById('example').innerText = example;
